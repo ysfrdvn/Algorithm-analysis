1edb90cf5a72922d9e0a3c78fd4c6e5b063b1797
Yusuf Rıdvan SAVUT <ysavut@stu.comu.edu.tr>
2019-02-22 14:52:28 +0300

    Update Logn-complexity_exponent-recurive.py

--- Logn-complexity_exponent-recurive.py
+++ Logn-complexity_exponent-recurive.py
@@ -9,3 +9,11 @@ def power_recursive(x,y):
         return power_recursive(x*x,int(y/2)) * x  #logn karmaşıklık
 
 power_recursive(2,999)        
+
+def call_report_rec(x,y):
+    global sayac
+    sayac = 0
+    r = power_recursive(x,y)
+    print(x, " üzeri ",y, " değeri : ",r, " çağrım sayisi : ", sayac)
+
+call_report_rec(2,999)    
